6b19f9d05bd532c3ee5a87d18875af1cf186ae43
EstebanBeltr4n <eescarraga@unicauca.edu.co>
2024-04-03 00:21:26 -0500

    +Organizacion del Preview para la PantallaPreview

--- app/src/main/java/com/example/screenoneempcauca/ListActivity.kt
+++ app/src/main/java/com/example/screenoneempcauca/ListActivity.kt
@@ -3,9 +3,13 @@ package com.example.screenoneempcauca
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,8 +47,22 @@ class ListActivity : ComponentActivity(){
 
 @Composable
 fun Pantalla(datos: List<String>){
+    LazyColumn() {
+        items(datos){
+            item ->ListItemRow(item)
+        }
+
+    }
 
 
+}
+
+@Composable
+fun ListItemRow(item: String){
+    Box (){
+        Text(text = item)
+
+    }
 }
 
 @Preview
