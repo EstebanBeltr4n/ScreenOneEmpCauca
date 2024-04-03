3117d2ced98bbdab92fcb2b98a60b1cde58f553d
EstebanBeltr4n <eescarraga@unicauca.edu.co>
2024-04-03 00:33:00 -0500

    Se agrega color a la lista de fondo

--- app/src/main/java/com/example/screenoneempcauca/ListActivity.kt
+++ app/src/main/java/com/example/screenoneempcauca/ListActivity.kt
@@ -3,8 +3,10 @@ package com.example.screenoneempcauca
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +14,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.screenoneempcauca.ui.theme.ScreenOneEmpCaucaTheme
 
@@ -59,13 +62,18 @@ fun Pantalla(datos: List<String>){
 
 @Composable
 fun ListItemRow(item: String){
-    Box (){
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(shape = MaterialTheme.shapes.small)
+            .background(color = MaterialTheme.colorScheme.secondary)
+    ){
         Text(text = item)
 
     }
 }
 
-@Preview
+@Preview (showBackground = true)
 @Composable
 fun PantallaPreview(){
     val datos : List<String> = listOf(
