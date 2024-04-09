cbb64e6516de74d3e2981f86400d5624c8e44512
EstebanBeltr4n <eescarraga@unicauca.edu.co>
2024-04-09 15:27:05 -0500

    Falta crear el vinculo entre las pantallas y con el menu desplegable

--- app/src/main/java/com/example/screenoneempcauca/LoginScreen.kt
+++ app/src/main/java/com/example/screenoneempcauca/LoginScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -28,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -64,13 +66,23 @@ fun LoginApp(){
         ) {
         Image(painter = painterResource(id =R.drawable.logo_png ),
             contentDescription = null)
-        Image(
-            painter = painterResource(id = R.drawable.feriapopayan),
-            contentDescription = null,
+        Box(
             modifier = Modifier
                 .size(width = 500.dp, height = 500.dp)
-                .clip(RoundedCornerShape(10.dp))
-        )
+                .clip(RoundedCornerShape(0.dp))
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.feriapopayan),
+                contentDescription = "publicidad",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .height(300.dp)
+                    .width(300.dp)
+                    .clip(RoundedCornerShape(16.dp))
+            )
+        }
+
 
         Spacer(modifier = Modifier.size(20.dp))
 
